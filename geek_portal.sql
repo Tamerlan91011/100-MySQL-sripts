d4d4420d3007627e1c42494ceb6bbf66385a3b80
Tamerlan91011 <timurglazunov@mail.ru>
2021-05-18 21:03:11 +0300

    Added scripts called `Update incorrect records`

--- geek_portal.sql
+++ geek_portal.sql
@@ -1,5 +1,5 @@
--- 15 ЗАПРОСОВ ИЗ ФУНКЦИОНАЛЬНЫХ ТРЕБОВАНИЙ 
--- ТРАНЗАКЦИОННЫЕ ЗАПРОСЫ:
+-- 15 ЗАПРОСОВ ИЗ ФУНКЦИОНАЛЬНЫХ ТРЕБОВАНИЙ --
+-- ТРАНЗАКЦИОННЫЕ ЗАПРОСЫ -- 
 -- 1.Добавить статью на форум
 use `geek_portal`;
 select * from `article`;
@@ -12,6 +12,7 @@ insert into `fandom` (`idfandom`,`category`,`name`) value (11,'videogames','Jojo
 delete from `fandom` where `idfandom` = 11;
 alter table `fandom` auto_increment = 10;
 insert into `forum` (`idforum`,`section`,`name`,`fandom_id`) value (11,'Guides','Jojo: Golden Eye',9);
+
 -- 3.Добавить нового персонажа
 insert into `character` (`id`,`rchat_id`,`user_id`,`name`,`initials`,`life_status`,`description`) 
 value (10,6,6,'Johnny Joestar','JoJo',1,'Cool dude who use power of spin. Standuser. Name of stand: Tusk');
@@ -28,7 +29,7 @@ and `life_status` = 1
 and `description` = 'President of USA'
 where `id` = 9;
 
--- СПРАВОЧНЫЕ ЗАПРОСЫ:
+-- СПРАВОЧНЫЕ ЗАПРОСЫ -- 
 -- 6.Показать новости текущего фандома (с учетом изменений имеется ввиду событие (event))
 select * from `event` where fandom_id = 1;
 
@@ -51,7 +52,7 @@ where user_id = iduser
 and role_game_event_id = idrole_game_event;
 
 
--- АНАЛИТИЧЕСКИЕ ЗАПРОСЫ:
+-- АНАЛИТИЧЕСКИЕ ЗАПРОСЫ -- 
 -- 11. Показать, сколько статей в выбранном фандоме (Фандоме "Stell ball run")
 SELECT COUNT(*) as 'Articles in fandom',`name` 
 FROM geek_portal.forum, geek_portal.fandom 
@@ -76,3 +77,25 @@ select count(`rchat_id`)/count(distinct `rchat_id`) from `using_tool`;
 
 -- 15. Среднее количество персонажей, созданных одним пользователем.
 select count(`id`)/count(distinct`user_id`) as 'avg num of chars to user' from geek_portal.character;
+
+-- ПРОЧИЕ ЗАПРОСЫ -- 
+-- Изменение некорректных данных (UPDATE)
+UPDATE `geek_portal`.`article` 
+SET `text` = 'Let me introduce you my comrads' 
+WHERE (`idarticle` = '8');
+
+UPDATE `geek_portal`.`article` 
+SET `text` = 'I\'m Jack Hidden, and this is my blog.' 
+WHERE (`idarticle` = '9');
+
+UPDATE `geek_portal`.`article` 
+SET `title` = 'Lifehacks in "Jojo: Golden Eye" Part 1', `text` = 'Bon Jiorno, my friends! I\'m Coule Joestar.' 
+WHERE (`idarticle` = '11');
+
+UPDATE `geek_portal`.`article` 
+SET `title` = 'Lifehacks in "Jojo: Golden Eye" Part 2', `text` = 'Bon Jiorno, my friends! ' 
+WHERE (`idarticle` = '12');
+
+UPDATE `geek_portal`.`chat` 
+SET `chat_name` = 'Discord pals from Server' 
+WHERE (`idchat` = '10');
